3855f3597c6a95ab2ba744b382fe047985d8c4dc
Daniel Probst <daenuprobst@gmail.com>
2021-07-03 09:44:08 +0200

    Added version

--- src/drfp/cli.py
+++ src/drfp/cli.py
@@ -7,11 +7,34 @@ from drfp import DrfpEncoder
 @click.command()
 @click.argument("input_file", type=click.File("r"))
 @click.argument("output_file", type=click.File("wb+"))
-@click.option("--n_folded_length", "-d", default=2048)
-@click.option("--min_radius", "-m", default=0)
-@click.option("--radius", "-r", default=3)
-@click.option("--rings/--no-rings", default=True)
-@click.option("--mapping/--no-mapping", default=False)
+@click.option(
+    "--n_folded_length",
+    "-d",
+    default=2048,
+    help="The lenght / dimensionality of the fingerprint. Good values are between 128 and 2048.",
+)
+@click.option(
+    "--min_radius",
+    "-m",
+    default=0,
+    help="The minimum radius used to extract circular substructures from molecules. 0 includes single atoms",
+)
+@click.option(
+    "--radius",
+    "-r",
+    default=3,
+    help="The radius, or maximum radius used to extract circular substructures form molecules.",
+)
+@click.option(
+    "--rings/--no-rings",
+    default=True,
+    help="Whether or not to extract whole rings as substructures.",
+)
+@click.option(
+    "--mapping/--no-mapping",
+    default=False,
+    help="Whether or not to also export a mapping to help interpret the fingerprint.",
+)
 def main(
     input_file: TextIO,
     output_file: TextIO,
@@ -21,6 +44,12 @@ def main(
     rings: True,
     mapping: False,
 ):
+    """Creates fingerprints from a file containing one reaction SMILES per line.
+
+    INPUT_FILE is the file containing one reaction SMILES per line.
+
+    OUTPUT_FILE will be a pickle file containing the corresponding list of fingerprints. If mapping is chosen, an addition file with the suffix .map will be created.
+    """
     smiles = []
     for line in input_file:
         smiles.append(line.strip())
